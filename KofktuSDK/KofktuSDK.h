bc3aed193411baf251ec856be4eec08dc50a72d6
Kofktu <kofktu@Kofktuui-MacBook-Pro-2.local>
2016-07-06 23:15:50 +0900

    프레임워크 프로젝트 설정

---
+++ KofktuSDK/KofktuSDK.h
@@ -0,0 +1,19 @@
+//
+//  KofktuSDK.h
+//  KofktuSDK
+//
+//  Created by Kofktu on 2016. 7. 6..
+//  Copyright © 2016년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KofktuSDK.
+FOUNDATION_EXPORT double KofktuSDKVersionNumber;
+
+//! Project version string for KofktuSDK.
+FOUNDATION_EXPORT const unsigned char KofktuSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KofktuSDK/PublicHeader.h>
+
+
