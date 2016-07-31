579d1ff74472f10e098a7d243f2fe671e6a8dae7
Kofktu <kofktu@Kofktuui-MacBook-Pro-2.local>
2016-07-31 23:00:02 +0900

    예제 프로젝트 추가

---
+++ KofktuSDKEx/KofktuSDKEx/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  KofktuSDKEx
+//
+//  Created by Kofktu on 2016. 7. 31..
+//  Copyright © 2016년 Kofktu. All rights reserved.
+//
+
+import UIKit
+import KofktuSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
