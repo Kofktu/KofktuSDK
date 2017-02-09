e654f68cbf9183c9d535626cb06de7537066e96e
Kofktu <kofktu@gmail.com>
2017-02-10 00:22:59 +0900

    KUILineView init method override

--- KofktuSDK/Classes/Components/KUILineView.swift
+++ KofktuSDK/Classes/Components/KUILineView.swift
@@ -43,6 +43,14 @@ open class KUILineView: UIView {
         }
     }
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     open override func draw(_ rect: CGRect) {
