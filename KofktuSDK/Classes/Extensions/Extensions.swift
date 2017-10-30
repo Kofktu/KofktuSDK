4e43d4c9df77f872881c8c8e77a4d32f98ac440a
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-10-30 13:15:28 +0900

    Xcode9 & Swift4 Migration

--- KofktuSDK/Classes/Extensions/Extensions.swift
+++ KofktuSDK/Classes/Extensions/Extensions.swift
@@ -43,7 +43,7 @@ public extension Array {
         for i in 0 ..< (count - 1) {
             let j = Int(arc4random_uniform(UInt32(count - i))) + i
             guard i != j else { continue }
-            swap(&self[i], &self[j])
+            swapAt(i, j)
         }
     }
     
