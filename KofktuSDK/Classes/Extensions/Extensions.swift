1e1c7c0236f2578554567389dabf966ad47b0a67
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-12-05 15:25:25 +0900

    String+BoundingRect Extension 함수추가

--- KofktuSDK/Classes/Extensions/Extensions.swift
+++ KofktuSDK/Classes/Extensions/Extensions.swift
@@ -221,6 +221,31 @@ public extension String {
         return numberOfLines
     }
     
+    public func boundingRect(with size: CGSize, attributes: [NSAttributedStringKey: Any]) -> CGRect {
+        let options: NSStringDrawingOptions = [.usesLineFragmentOrigin, .usesFontLeading]
+        let rect = self.boundingRect(with: size, options: options, attributes: attributes, context: nil)
+        return snap(rect)
+    }
+    
+    public func size(thatFits size: CGSize, font: UIFont, maximumNumberOfLines: Int = 0) -> CGSize {
+        let attributes: [NSAttributedStringKey: Any] = [.font: font]
+        var size = self.boundingRect(with: size, attributes: attributes).size
+        if maximumNumberOfLines > 0 {
+            size.height = min(size.height, CGFloat(maximumNumberOfLines) * font.lineHeight)
+        }
+        return size
+    }
+    
+    public func width(with font: UIFont, maximumNumberOfLines: Int = 0) -> CGFloat {
+        let size = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+        return self.size(thatFits: size, font: font, maximumNumberOfLines: maximumNumberOfLines).width
+    }
+    
+    public func height(thatFitsWidth width: CGFloat, font: UIFont, maximumNumberOfLines: Int = 0) -> CGFloat {
+        let size = CGSize(width: width, height: CGFloat.greatestFiniteMagnitude)
+        return self.size(thatFits: size, font: font, maximumNumberOfLines: maximumNumberOfLines).height
+    }
+    
 }
 
 public extension Data {
